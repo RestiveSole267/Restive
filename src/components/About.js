4269c3018b534de3d7b770459d3722dc3d9e283b
Cameron White <cameronwhite35288@gmail.com>
2022-08-18 18:06:58 -0400

    0.1.1 - avia
    Updating the about me information & photo.

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,7 @@ import React from "react";
 
 import "./About.css";
 import FadeInSection from "./FadeInSection";
-import Restive from "./assets/restive.jpeg";
+import Restive from "./assets/restive2.png";
 
 class About extends React.Component {
     constructor() {
@@ -27,14 +27,12 @@ class About extends React.Component {
                     </div>
                     <div class="about-content">
                         <div class="about-desc">
-                            <p>Hi, I'm Cameron. I'm currently a senior in high school with an passion in <b>Computer Science</b>, <b>Psychology</b>, and <b>Political Science</b>.</p>
-                            <p>Most of my childhood I took things apart items to figure out how they worked. My mama always told me that I've always been someone who was very good with their hands, an intuitive thinker, and that I've always had creative ideas.</p>
-                            <p>Around 2014, I started a YouTube channel called <a href='https://www.youtube.com/channel/UCxwa8a5iSx22UUcnQmCp1Yg'>RestiveSole267</a>, and later I started another one called <a href='https://www.youtube.com/c/RestiveToo'>RestiveToo</a>. At the age of 11, I learned my first programming language, Python, and began creating Discord clients.</p>
-                            <p>My freshman year of high school, I spent my mornings with Mrs. Stone, my AP US Government and Politics teacher. We always talk about Politics as we always listen to a radio show every morning that was based in Tallahassee. We also talked about mental disorder, and in particular, we tended to talk about ADHD which is a mental disorder I was diagnose with, and I grew a love for Psychology and Political Science.</p>
-                            <p>When I'm not studying or working on projects, I'm usually hanging out with my online or local friends, editing videos, or watching YouTube videos.</p>
+                            <p>Hi, I'm Cameron. I'm currently a first year Computer Science major at <a href='https://www.kennesaw.edu/about/index.php'>Kennesaw State University</a>. I currently have experience in programming languages such as <b>Python</b>, <b>CSS</b>, <b>HTML</b>, and <b>Javascript</b>.</p>
+                            <p>My mama constantly told me that I was always intuitive thinker, someone who could quickly generate creative thoughts, and someone who was skilled with their hands. I grew up around computers for the most of my childhood, I started my first YouTube channel, <a href='https://www.youtube.com/channel/UCxwa8a5iSx22UUcnQmCp1Yg'>RestiveSole267</a>, in 2014. Later, I launched <a href='https://www.youtube.com/c/RestiveToo'>RestiveToo</a>, in 2016. When I reached eleven, I first learned how to program in Python, which most of my Discord Clients are programmed in Python.</p>
+                            <p>Outside of studying or working, I typically spend my free time with friends, editing videos, playing video games, streaming, working out, or watching shows on Netflix, Hulu, or YouTube.</p>
                         </div>
                         <div class="about-image">
-                            <img src={Restive} width="220" height="240" className="Restive" alt="Restive.jpeg"></img>    
+                            <img src={Restive} width="300" height="240" className="Restive" alt="Restive2.png"></img>    
                         </div>
                     </div>
                 </FadeInSection>
